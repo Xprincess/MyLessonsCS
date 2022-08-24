a8feed24733c3c76a5c4a48e2408108e2d5237a6
NastyaAverkina <anastasia.averkina@gmail.com>
2022-08-24 13:17:33 +0300

    add Задача 13

--- Sem2HW/Program.cs
+++ Sem2HW/Program.cs
@@ -4,6 +4,7 @@
 // 782 -> 8
 // 918 -> 1
 
+/*
 int FindSecdigit (int number)//456
 {
     int ed = number / 10;//456-убирется 6
@@ -20,11 +21,32 @@ Console.WriteLine($"Secdigit is {Secdig}");
 
 //Программа не работает,,
 
-
+*/
 
 //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
 // //что третьей цифры нет.
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+/*
+void ThirdDigit(int number)
+{
+    string numberText = Convert.ToString(number);
+    if (numberText.Length > 2)
+        Console.WriteLine("The third digit is " + numberText[2]);
+    else
+        Console.WriteLine("The third digit doesn't exist");
+}
+Console.Write("Insert number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+ThirdDigit(num);
+
+*/
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
 
